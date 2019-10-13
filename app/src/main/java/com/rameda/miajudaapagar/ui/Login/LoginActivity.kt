88e85e8e2116f2c817fdc116bf248047919ecfab
adema <ademarxii@gmail.com>
2019-10-13 01:35:45 -0300

    commit ajust login layout

--- app/src/main/java/com/rameda/miajudaapagar/ui/Login/LoginActivity.kt
+++ app/src/main/java/com/rameda/miajudaapagar/ui/Login/LoginActivity.kt
@@ -11,18 +11,20 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.R
-
+import android.graphics.Color
 
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow()?.setStatusBarColor(Color.BLACK)
+
         setContentView(com.rameda.miajudaapagar.R.layout.activity_login)
         supportActionBar?.hide()
 
         angry_btn.setOnClickListener {
-   //         startActivity(Intent(this,MainActivity::class.java))
+         startActivity(Intent(this,MainActivity::class.java))
             // Write a message to the database
             val database = FirebaseDatabase.getInstance()
             val myRef = database.getReference("message")
@@ -30,5 +32,7 @@ class LoginActivity : AppCompatActivity() {
             myRef.setValue("Hello, World!")
 
         }
+
+
     }
 }
