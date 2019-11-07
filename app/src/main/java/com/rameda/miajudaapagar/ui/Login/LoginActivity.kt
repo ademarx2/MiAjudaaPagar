b313764579563f638c598deb217a41d37fdcb319
AdemarNeto <ademar.neto@usemobile.xyz>
2019-11-07 00:50:51 -0300

    ajust in layout and init back firebase

--- app/src/main/java/com/rameda/miajudaapagar/ui/Login/LoginActivity.kt
+++ app/src/main/java/com/rameda/miajudaapagar/ui/Login/LoginActivity.kt
@@ -7,11 +7,17 @@ import com.rameda.miajudaapagar.MainActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.R
 import android.graphics.Color
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import android.widget.Toast
 
 
 class LoginActivity : AppCompatActivity() {
@@ -22,16 +28,46 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(com.rameda.miajudaapagar.R.layout.activity_login)
         supportActionBar?.hide()
-
+        var a = 0
         angry_btn.setOnClickListener {
-         startActivity(Intent(this,MainActivity::class.java))
+         //startActivity(Intent(this,MainActivity::class.java))
             // Write a message to the database
             val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("message")
+            val myRef = database.getReference("Group")
+            a=a+1
+            val myRef2 = database.getReference("Status")
+            myRef2.setValue("ativo")
 
-            myRef.setValue("Hello, World!")
+            myRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    val value = dataSnapshot.getValue(String::class.java)
+                    Log.d("opa", "Value is: " + value!!)
+                    if (value.toInt()<=4){
+                        myRef.setValue(a.toString())
+                    }
+                    else{
+                        Toast.makeText(applicationContext,"PARTY FULL",Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                    Log.w("opa", "Failed to read value.", error.toException())
+                }
+            })
+
 
         }
+    }
+
+    fun onDataCharge(dataSnapShot:DataSnapshot){
+        var map = mapOf<String,String>()
+        map = dataSnapShot.getValue() as Map<String, String>
+
+        val Status = map.get("lotado")
+        val NumPart = map.get("2")
 
 
     }
